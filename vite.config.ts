a55d02eae0e9db6267d4cb92b001735603615319
Amukta Yajamanyam <151421549+amukta14@users.noreply.github.com>
2025-03-10 22:47:07 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, // Change if needed
-  },
-});
+  base: '/Gaming-Arena/', // 🔥 Set this to your repo name with a slash
+})
